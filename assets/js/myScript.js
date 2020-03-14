a2a552b9b2a8bff2c76365bf7d78ffa001655774
Ike Barranco <barranco.ike@outlook.com>
2020-03-13 21:37:51 -0600

    Fixed javascript to check for users answer and created loops to ensure that user inputs correct information

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -4,30 +4,52 @@ let specChars = ['!','?','@','#','$','%','^','&','*','(',')','<','>','~','`'];
 let numbers= ['0','1','2','3','4','5','6','7','8','9'];
 
 let passLength;
+let MIN_LENGTH = 8;
+let MAX_LENGTH = 128;
 
 //Function To Be Able to Generate Password
 function createPassword()
 {
-    let validInput = true;
+    let validInput = false;
+
+    alert("test");
+    
 
     //Make User Input A Number
-    While(validInput === true)
+    while(validInput === false)
     {
-        //Ask User How Long They want Thier Password to Be
-        passLength = prompt("How Long Will Your Password Need To Be? (8-126");
 
-        //Checks To See If User Input is a number
+        //Ask User How Long They want Thier Password to Be
+        passLength = prompt("How Long Will Your Password Need To Be? (8-126 Characters)");
+
+        
+
+        //Checks To See If User Input isn't a number
         if(isNaN(passLength))
-        {
-            //Turn User Input Into A Number
-            passLength = parseInt(passLength);
-        }
-        //User Input Isnt A Number
-        else
         {
             //Tell User That They Suck!
             alert("Input Wasn't a number");
         }
+        //User Input Is A
+        else
+        {
+            //Turn User Input Into A Number
+            passLength = parseInt(passLength);
+            
+            //Check if User Input is inbwetween parameters
+            if(passLength >= MIN_LENGTH && passLength <= MAX_LENGTH)
+            {
+                //Set User input to be true
+                validInput = true;
+            }
+            else
+            {
+                //Tell User Answer Is Out Of Bound
+                alert("Password Must Be between 8 and 128. Please Re-enter your choice");
+            }
+
+        }
+
     }
 
 }
