0362f07915888000b061dbbeb9449f8b39ee80b6
Ike Barranco <barranco.ike@outlook.com>
2020-03-13 22:25:12 -0600

    Created Functio To Check User Input To Reduce Size Of createPassword Function

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -2,7 +2,9 @@
 let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let specChars = ['!','?','@','#','$','%','^','&','*','(',')','<','>','~','`'];
 let numbers= ['0','1','2','3','4','5','6','7','8','9'];
+let allChoices = [letters, specChars, numbers];//All arrays in one list
 
+let password;
 let passLength;
 let MIN_LENGTH = 8;
 let MAX_LENGTH = 128;
@@ -22,34 +24,53 @@ function createPassword()
         //Ask User How Long They want Thier Password to Be
         passLength = prompt("How Long Will Your Password Need To Be? (8-126 Characters)");
 
-        
-
-        //Checks To See If User Input isn't a number
-        if(isNaN(passLength))
-        {
-            //Tell User That They Suck!
-            alert("Input Wasn't a number");
-        }
-        //User Input Is A
-        else
-        {
-            //Turn User Input Into A Number
-            passLength = parseInt(passLength);
-            
-            //Check if User Input is inbwetween parameters
-            if(passLength >= MIN_LENGTH && passLength <= MAX_LENGTH)
-            {
-                //Set User input to be true
-                validInput = true;
-            }
-            else
-            {
-                //Tell User Answer Is Out Of Bound
-                alert("Password Must Be between 8 and 128. Please Re-enter your choice");
-            }
-
-        }
-
+        //Check User Input
+        validInput = checkNumberInput(passLength);
     }
+
+    //Turn String to int
+    passLength = parseInt(passLength);
+
+    //Start Generating password
+    for(let i = 0; i < passLength; i++)
+    {
+        //Choose which choice to pick
+
+        //if letter choose upper or lower chase
+
+        //
+    }
+    
+
+}
+
+
+
+function checkNumberInput(length)
+{
+     //Checks To See If User Input isn't a number
+     if(isNaN(length))
+     {
+         //Tell User That They Suck!
+         alert("Input Wasn't a number");
+     }
+     //User Input Is A
+     else
+     {
+         //Turn User Input Into A Number
+         length = parseInt(length);
+         
+         //Check if User Input is inbwetween parameters
+         if(length >= MIN_LENGTH && length <= MAX_LENGTH)
+         {
+            return true;
+         }
+     }
+
+    //Tell User they Suck!
+    alert("Input must be between 8 and 128 characters")
+    
+    //User input isnt inbetween the parameters
+    return false;
 
 }
